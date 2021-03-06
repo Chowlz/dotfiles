2a50c329320feccd60a2ecd4b780761ec7002ee8
Charles Cruz <mail@charlescruz.dev>
2021-03-05 18:47:50 -0800

    [t/c/nixpkgs/home.nix] Update for macos

--- tag-base/config/nixpkgs/home.nix
+++ tag-base/config/nixpkgs/home.nix
@@ -2,6 +2,7 @@
 
 let
   unstable = import <nixpkgs-unstable> {};
+  stdenv = pkgs.stdenv;
 in {
   home.username = "ccruz";
   home.homeDirectory = "/home/ccruz";
@@ -9,20 +10,44 @@ in {
   home.packages = [
     unstable.awscli
     unstable.bat
-    unstable.bat-extras.batman
     unstable.bat-extras.batdiff
     unstable.bat-extras.batgrep
+    unstable.bat-extras.batman
     unstable.bat-extras.batwatch
     unstable.clj-kondo
     unstable.clojure
+    unstable.diceware
     unstable.exa
-    unstable.bpytop
-    unstable.emacs
+    unstable.lastpass-cli
     unstable.neofetch
     unstable.neovim
     unstable.nodejs
+    unstable.pastel
+    unstable.python39
     unstable.rcm
+    unstable.ripgrep
     unstable.rlwrap
+  ] ++ lib.optionals stdenv.isDarwin [
+    unstable.coreutils
+    unstable.curl
+    unstable.diffutils
+    unstable.findutils
+    unstable.gawk
+    unstable.gnugrep
+    unstable.gnused
+    unstable.gnutar
+    unstable.gzip
+    unstable.keychain
+    unstable.lzma
+    unstable.mosh
+    unstable.tmux
+    unstable.wget
+    unstable.which
+    unstable.zsh
+    unstable.zsh-powerlevel10k
+  ] ++ lib.optionals stdenv.isLinux [
+    unstable.bpytop
+    unstable.emacs
     unstable.shellcheck
   ];
 
