96557341a356b961069fa99fc923409d70efee46
Charles Cruz <mail@charlescruz.dev>
2021-03-16 00:40:07 -0700

    [setup.sh] Refactor and make universal for different rcrc files

--- setup.sh
+++ setup.sh
@@ -2,13 +2,50 @@
 
 set -eu
 
-cd "$HOME"
+RCRC="${1:-$HOME/.dotfiles/rcrc}"
+NODEJS_MODULES=(es-lint shadow-cljs source-map-support tslint typescript ws)
 
-# Nix
-nix-channel --update
+prompt () {
+  echo -e "\033[0;35m$*\033[0m"
+}
+
+question () {
+  local question_prompt="$1"
+  local action="$2"
+  while true; do
+    read -p "$question_prompt (y/n) " -r yn
+    case $yn in
+      [Yy]) eval "$action" && break ;;
+      [Nn]) echo -e "\033[1;33mSkipping\033[0m" && break ;;
+      * )   echo "Please answer yes or no." ;;
+    esac
+  done
+}
+
+# Dotfiles
+setup-dotfiles () {
+  prompt "Setting up dotfiles - rcrc directory: $RCRC"
+  local dotfiles_dir
+  dotfiles_dir=$(dirname "$RCRC")
+  if which lsrc &> /dev/null; then
+    RCRC=$RCRC lsrc | sort | grep --color -E "$dotfiles_dir|$"
+    question "Apply dotfiles?" "RCRC=$RCRC rcup"
+  else
+    nix-env -iA nixpkgs.rcm
+    RCRC=$RCRC lsrc | sort | grep --color -E "$dotfiles_dir|$"
+    question "Apply dotfiles?" "RCRC=$RCRC rcup"
+    nix-env -e rcm
+  fi
+}
+
+# Home-manager
+setup-home-manager () {
+  prompt "Setting up home-manager"
+  question "Update home-manager?" "nix-channel --update && home-manager switch"
+}
 
 # Doom-Emacs
-setup-emacs () {
+install-emacs () {
   rm -fr ~/.emacs.d
   git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
   ~/.emacs.d/bin/doom install
@@ -16,21 +53,29 @@ setup-emacs () {
   ~/.emacs.d/bin/doom build
 }
 
-echo "Setting up doom-emacs .emacs.d"
-if [ -L "$HOME/.emacs.d" ] || [ -d "$HOME/.emacs.d" ]; then
-  while true; do
-    read -pr ".emacs.d detected - skip? (yes/no)" yn
-      case $yn in
-          [Yy]es) setup-emacs ;;
-          [Nn]o)  echo "Skipping" && break ;;
-          * )     echo "Please answer yes or no." ;;
-      esac
-  done
-else
-  setup-emacs
-fi
+setup-emacs () {
+  prompt "Setting up doom-emacs .emacs.d"
+  if [ -L "$HOME/.emacs.d" ] || [ -d "$HOME/.emacs.d" ]; then
+    question ".emacs.d detected - skip?" install-emacs
+  else
+    install-emacs
+  fi
+}
 
-# Nodejs deps
-# setup-npm () {
-#   npm install -g es-lint shadow-cljs source-map-support tslint typescript ws
-# }
+# Nodejs
+# shellcheck disable=SC2145
+setup-nodejs () {
+  prompt "Setting up nodejs"
+  echo "Modules: ${NODEJS_MODULES[@]}"
+  question "Install npm modules?" "npm install -g ${NODEJS_MODULES[@]}"
+}
+
+# Bootstrap
+cd "$HOME"
+
+setup-dotfiles
+setup-home-manager
+setup-emacs
+setup-nodejs
+
+echo -e "\033[0;32mDone!\033[0m"
