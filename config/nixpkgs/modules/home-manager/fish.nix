4b9e3a91e58c5bf5973c49d4d9321b25931297b6
Charles Cruz <mail@charlescruz.dev>
2021-10-05 17:11:30 +0000

    [c/n/m/home-manager/fish.nix] Add npm-path

--- config/nixpkgs/modules/home-manager/fish.nix
+++ config/nixpkgs/modules/home-manager/fish.nix
@@ -354,6 +354,7 @@ in {
               str =
                 let
                   bin-path = "${config.home.homeDirectory}/bin";
+                  npm-path="${config.home.homeDirectory}/.local/share/npm/bin";
                   nix = babelfish "${pkgs.nix}/etc/profile.d/nix.sh" "nix";
                   nix-channels-path = "${config.home.homeDirectory}/.nix-defexpr/channels";
                   nix-root-channels-path = "/nix/var/nix/profiles/per-user/root/channels";
@@ -365,6 +366,9 @@ in {
                     and contains -i ${nix-root-channels-path} $NIX_PATH &> /dev/null
                     and set -e NIX_PATH[(contains -i ${nix-root-channels-path} $NIX_PATH)]
                     and set -gx NIX_PATH $NIX_PATH
+                  test -e ${npm-path}
+                    and ! contains -i ${npm-path} $PATH &> /dev/null
+                    and set -gx PATH ${npm-path} $PATH
                   test -e ${bin-path}
                     and ! contains -i ${bin-path} $PATH &> /dev/null
                     and set -gx PATH ${bin-path} $PATH
