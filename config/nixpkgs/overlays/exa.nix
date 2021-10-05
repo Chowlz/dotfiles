8ee740bf0b82cf0cc74450780b7ce17814bf5e99
Charles Cruz <mail@charlescruz.dev>
2021-10-05 20:07:44 +0000

    [c/n/overlays/exa.nix] Update exa

--- config/nixpkgs/overlays/exa.nix
+++ config/nixpkgs/overlays/exa.nix
@@ -3,8 +3,8 @@ self: super: {
     src = super.fetchFromGitHub {
       owner = "ogham";
       repo = "exa";
-      rev = "95682f567461beca22198b29a7abf9411f41189f";
-      sha256 = "1abbk56xvaif5vsl6z4arr9jj8cxnfklql43215hckl0b7v2hhj5";
+      rev = "a7aca35d977c52557fe4c8d5dbeba68990e402ac";
+      sha256 = "1cjqmf03pwmbclzwcnk0ykj1cmai0qd73rnvh5y8q1kq7qxbs2y7";
     };
 
     postPatch = super.lib.optionalString (! super.stdenv.isDarwin) ''
@@ -12,11 +12,22 @@ self: super: {
       --replace "/usr/share/zoneinfo" "/etc/zoneinfo"
     '';
 
+    postInstall = ''
+      pandoc --standalone -f markdown -t man man/exa.1.md > man/exa.1
+      pandoc --standalone -f markdown -t man man/exa_colors.5.md > man/exa_colors.5
+      installManPage man/exa.1 man/exa_colors.5
+      installShellCompletion \
+        --bash --name exa completions/bash/exa \
+        --fish --name exa.fish completions/fish/exa.fish \
+        --zsh --name _exa completions/zsh/_exa
+    '';
+
+
     patches = [ ];
 
     cargoDeps = old.cargoDeps.overrideAttrs (super.lib.const {
       inherit src patches;
-      outputHash = "154vb2l6778iriq6s4yix8sy2sl21jk87rmkyfrmjaa4cbqprj90";
+      outputHash = "0y1czqsr5wfbpg671970lx4dmxk158h27hrynyai7y9vss77i82f";
     });
   });
 }
