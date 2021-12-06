ee90106a436fede3c539ac5b23355a8bb11d0b87
Charles Cruz <mail@charlescruz.dev>
2021-12-06 18:53:11 +0000

    [doom.d/init.el] Update to init.el from latest example

--- doom.d/init.el
+++ doom.d/init.el
@@ -29,6 +29,7 @@
        (helm +fuzzy)                    ; the *other* search engine for love and life
        ;;ido                            ; the other *other* search engine...
        ;;ivy                            ; a search engine for love and life
+       ;;vertico                        ; the search engine of the future
 
        :ui
        ;;deft                           ; notational velocity for Emacs
@@ -36,7 +37,6 @@
        doom-dashboard                   ; a nifty splash screen for Emacs
        doom-quit                        ; DOOM quit-message prompts when you quit Emacs
        (emoji +unicode)                 ; 🙂
-       ;;fill-column                    ; a `fill-column' indicator
        hl-todo                          ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides                  ; highlighted indent columns
@@ -90,6 +90,7 @@
 
        :tools
        ;;ansible
+       ;;biblio                         ; Writes a PhD for you (citation needed)
        ;;debugger                       ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
@@ -116,7 +117,8 @@
 
        :lang
        ;;agda                           ; types of types of types of types...
-       ;;cc                             ; C/C++/Obj-C madness
+       ;;beancount                      ; mind the GAAP
+       ;;cc                             ; C > C++ == 1
        clojure                          ; java with a lisp
        ;;common-lisp                    ; if you've seen one lisp, you've seen them all
        ;;coq                            ; proofs-as-programs
@@ -124,17 +126,19 @@
        ;;csharp                         ; unity, .NET, and mono shenanigans
        ;;data                           ; config/data formats
        ;;(dart +flutter)                ; paint ui and not much else
+       ;;dhall
        ;;elixir                         ; erlang done right
        ;;elm                            ; care for a cup of TEA?
        emacs-lisp                       ; drown in parentheses
        ;;erlang                         ; an elegant language for a more civilized age
        ;;ess                            ; emacs speaks statistics
+       ;;factor
        ;;faust                          ; dsp, but you get to keep your soul
        ;;fsharp                         ; ML stands for Microsoft's Language
        ;;fstar                          ; (dependent) types and (monadic) effects and Z3
        ;;gdscript                       ; the language you waited for
        ;;(go +lsp)                      ; the hipster dialect
-       ;;(haskell +dante)               ; a language that's lazier than I am
+       ;;(haskell +lsp)                 ; a language that's lazier than I am
        ;;hy                             ; readability of scheme w/ speed of python
        ;;idris                          ; a language you can depend on
        json                             ; At least it ain't XML
@@ -143,9 +147,8 @@
        ;;julia                          ; a better, faster MATLAB
        ;;kotlin                         ; a better, slicker Java(Script)
        ;;latex                          ; writing papers in Emacs has never been so fun
-       ;;lean
-       ;;factor
-       ;;ledger                         ; an accounting system in Emacs
+       ;;lean                           ; for folks with too much to prove
+       ;;ledger                         ; be audit you can be
        ;;lua                            ; one-based indices? one-based indices
        markdown                         ; writing docs for people to ignore
        ;;nim                            ; python + lisp at the speed of c
@@ -172,14 +175,16 @@
        ;;terra                          ; Earth and Moon in alignment for performance.
        ;;web                            ; the tubes
        yaml                             ; JSON, but readable
+       ;;zig                            ; C, but simpler
 
        :email
-       ;;(mu4e +gmail)
+       ;;(mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
        ;;calendar
+       ;;emms
        ;;everywhere                     ; *leave* Emacs!? You must be joking
        ;;irc                            ; how neckbeards socialize
        ;;(rss +org)                     ; emacs as an RSS reader
