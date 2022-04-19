d701e6e5ab51077ce00db480a619b221672cc325
Charles Cruz <mail@charlescruz.dev>
2022-04-18 20:02:36 -0700

    [*] Update README.md and setup.sh

--- README.md
+++ README.md
@@ -14,48 +14,6 @@ cd $DOTFILES_DIR
 git config user.email "mail@charlescruz.dev"
 ```
 
-## Setup homebrew (Mac only):
-Used to install the following
-- Rectangle
-- Spotify
-- Emacs 28.1
-```
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH="/opt/homebrew/bin:$PATH"
-brew install --cask rectangle
-brew install --cask spotify
-brew tap railwaycat/emacsmacport
-brew install emacs-mac
-```
-
-## Setup nix darwin (Mac only):
-```
-curl -L https://nixos.org/nix/install | sh
-nix-build https://github.com/LnL7/nix-darwin/archive master.tar.gz -A installer
-./result/bin/darwin-installer
-
-# Check build
-darwin-rebuild build -I darwin-config=$HOME/.config/nixpkgs/darwin-configuration.nix
-
-# Confirm changes
-darwin-rebuild switch-I darwin-config=$HOME/.config/nixpkgs/darwin-configuration.nix
-
-# Remove old .nixpkgs
-rm -fr .nixpkgs
-```
-
-## Setup home-manager:
-```
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-shell '<home-manager>' -A install
-```
-
-## Link gui apps (Mac only):
-```
-ln -s /opt/homebrew/opt/emacs-mac/Emacs.app /Applications
-ln -s /run/current-system/Applications/iTerm2.app /Applications
-```
-
 ## Bootstrap:
 ```
 ~/.dotfiles/setup.sh
--- setup.sh
+++ setup.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
 RCRC="${1:-$HOME/.dotfiles/rcrc}"
 NODEJS_MODULES=(es-lint shadow-cljs source-map-support tslint typescript ws)
+
+add-to-path () {
+  while [ -n "$1" ]; do
+    case ":$PATH:" in
+      *":$1:"*) :;;
+      *) PATH="$1:$PATH";;
+    esac
+    shift
+  done
+  export PATH
+}
 
 prompt () {
   echo -e "\033[0;35m$*\033[0m"
@@ -22,11 +33,45 @@ question () {
   done
 }
 
+# Homebrew (MacOs only)
+setup-homebrew () {
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    if ! which brew &> /dev/null; then
+      prompt "Installing homebrew"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    if [ ! -e /opt/homebrew/opt/emacs-mac/Emacs.app ]; then
+      question "Install emacs?" \
+        "brew tap railwaycat/emacsmacport && brew install emacs-mac && ln -s /opt/homebrew/opt/emacs-mac/Emacs.app /Applications"
+    fi
+
+    if [ ! -e /Applications/Rectangle.app ]; then
+      question "Install rectangle?" "brew install --cask rectangle"
+    fi
+
+    if [ ! -e /Applications/Spotify.app ]; then
+      question "Install spotify?" "brew install --cask spotify"
+    fi
+  fi
+}
+
+# Nix (MacOs only)
+setup-nix () {
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    if ! which nix-build &> /dev/null; then
+      prompt "Installing nix package manager"
+      curl -L https://nixos.org/nix/install | sh
+    fi
+  fi
+}
+
 # Dotfiles
 setup-dotfiles () {
   prompt "Setting up dotfiles - rcrc directory: $RCRC"
   local dotfiles_dir
   dotfiles_dir=$(dirname "$RCRC")
+
   if which lsrc &> /dev/null; then
     RCRC=$RCRC lsrc | sort | grep --color -E "$dotfiles_dir|$"
     question "Apply dotfiles?" "RCRC=$RCRC rcup"
@@ -38,14 +83,58 @@ setup-dotfiles () {
   fi
 }
 
+# Nix-darwin
+install-nix-darwin () {
+  nix-build https://github.com/LnL7/nix-darwin/archive master.tar.gz -A installer
+  if [ ! -e "/etc/shells.bak" ]; then
+    echo "Moving /etc/shells to /etc/shells.bak"
+    sudo mv /etc/shells /etc/shells.bak
+  fi
+  ./result/bin/darwin-installer
+  rm -fr result
+  darwin-rebuild switch -I darwin-config="$HOME/.config/nixpkgs/darwin-configuration.nix"
+  rm -fr "$HOME/.nixpkgs"
+  if [ -e  "/run/current-system/Applications/iTerm2.app" ]; then
+    ln -s /run/current-system/Applications/iTerm2.app /Applications
+  fi
+}
+
+update-nix-darwin () {
+  nix-channel --update
+  darwin-rebuild switch -I darwin-config="$HOME/.config/nixpkgs/darwin-configuration.nix"
+  if [ -e  "/run/current-system/Applications/iTerm2.app" ]; then
+    ln -s /run/current-system/Applications/iTerm2.app /Applications
+  fi
+}
+
+setup-nix-darwin () {
+  prompt "Setting up nix-darwin"
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    if ! which nix-env &> /dev/null; then
+      question "Install nix darwin?" install-nix-darwin
+    else
+      question "Update nix-darwin?" update-nix-darwin
+    fi
+  fi
+}
+
 # Home-manager
+install-home-manager () {
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-shell '<home-manager>' -A install
+}
+
 setup-home-manager () {
   prompt "Setting up home-manager"
-  question "Update home-manager?" "nix-channel --update && home-manager switch"
+  if ! which home-manager &> /dev/null; then
+    question "Install home-manager?" install-home-manager
+  else
+    question "Update home-manager?" "nix-channel --update && home-manager switch"
+  fi
 }
 
 # Doom-Emacs
-install-emacs () {
+install-doom-emacs () {
   if [ -L "$HOME/.emacs.d" ] || [ -d "$HOME/.emacs.d" ]; then
     cd "$HOME/.emacs.d"
     find . -name . -o -prune -exec rm -rf -- {} +
@@ -59,12 +148,12 @@ install-emacs () {
   "$HOME/.emacs.d/bin/doom" build
 }
 
-setup-emacs () {
+setup-doom-emacs () {
   prompt "Setting up doom-emacs .emacs.d"
   if [ -L "$HOME/.emacs.d" ] || [ -d "$HOME/.emacs.d" ]; then
-    question ".emacs.d detected - install?" install-emacs
+    question ".emacs.d detected - install?" install-doom-emacs
   else
-    install-emacs
+    install-doom-emacs
   fi
 }
 
@@ -79,9 +168,16 @@ setup-nodejs () {
 # Bootstrap
 cd "$HOME"
 
+add-to-path /Users/ccruz/.nix-profile/bin
+add-to-path /nix/var/nix/profiles/default/bin
+add-to-path /run/current-system/sw/bin
+
+setup-homebrew
+setup-nix
 setup-dotfiles
+setup-nix-darwin
 setup-home-manager
-setup-emacs
+setup-doom-emacs
 setup-nodejs
 
 echo -e "\033[0;32mDone!\033[0m"
